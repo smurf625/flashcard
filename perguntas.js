6300cda7f8a079260e3a16359c4b6f89c4a65f67
smurf625 <desouza.arthur07@gmail.com>
2024-11-06 07:46:20 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Pokemon',
+    'Qual foi o primeiro Pokémon do mundo?',
+    'O primeiro Pokémon do mundo foi Arceus. Isso porque ele nasceu e deu origem a todo o universo dos Pokémon.'
+)
+
+criaCartao(
+    'Pokemon',
+    'Quem inventou Pokémon?',
+    'Satoshi Tajiri'
+)
+
+criaCartao(
+    'Pokemon',
+    'Porque o nome Pokémon?',
+    'A palavra pokémon é a contração de duas palavras em inglês: pocket, que significa bolso; e monster, que significa monstro.'
+)
+
+criaCartao(
+    'Pokemon',
+    'Quantos Pokémon existem?',
+    'A franquia Pokémon gira em torno de 1015 espécies fictícias de monstros colecionáveis'
+)
